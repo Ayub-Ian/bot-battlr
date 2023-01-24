65d95ecb470afc5bf8a8a325a7be24fe5a6176f7
Ayub Ian <iayub.mia@gmail.com>
2023-01-25 00:29:01 +0300

    added delete button

--- src/components/bot-collection/BotCollectionItem.js
+++ src/components/bot-collection/BotCollectionItem.js
@@ -1,6 +1,16 @@
 import React from 'react'
 
+
 function BotCollectionItem({ bot, botFunction }) {
+
+    const botTypeClasses = {
+        Assault: require("../../assets/blade-bite.svg").default,
+        Defender: require("../../assets/layered-armor.svg").default,
+        Support: require("../../assets/bat-blade.svg").default,
+        Medic: require("../../assets/plague-doc.svg").default,
+        Witch: require("../../assets/witch-face.svg").default,
+        Captain: require("../../assets/air-man.svg").default,
+      }
 
     const { id, name, health, damage, armor, bot_class, catchphrase, avatar_url } = bot
   return (
@@ -10,8 +20,28 @@ function BotCollectionItem({ bot, botFunction }) {
           </div>
           <div className='title'>
             <h3>{name}</h3>
+            <img src={botTypeClasses[bot_class]} alt={name} />
           </div>
           <p>{catchphrase}</p>
+          <div className='divider'></div>
+          <div className='bottom'>
+          <div className='attributes'>
+            <div>
+                <img src={require('../../assets/health.svg').default} alt="health"/>
+                <p>{health}</p>
+            </div>
+            <div>
+                <img src={require('../../assets/lightning.svg').default} alt="damage"/>
+                <p>{damage}</p>
+            </div>
+            <div>
+                <img src={require('../../assets/chest-armor.svg').default} alt="armor"/>
+                <p>{armor}</p>
+            </div>
+          </div>
+
+          <button className='discharge'>X</button>
+          </div>
         </div>
   )
 }
